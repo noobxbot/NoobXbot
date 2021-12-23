9117bfd08f8e46a55a5ee9f09d6149475c34a5ce
Akku <96183105+userderdead@users.noreply.github.com>
2021-12-23 11:56:45 +0530

    fix

--- NoobXbot/__init__.py
+++ NoobXbot/__init__.py
@@ -53,7 +53,7 @@ async def load_sudoers():
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(load_sudoers()
-Music_START_TIME = time.time()
+NOOBXBOT_START_TIME = time.time()
 loop = asyncio.get_event_loop()
 
 
